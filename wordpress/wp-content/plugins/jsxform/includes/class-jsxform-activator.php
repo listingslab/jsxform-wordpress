957e049a315c5e33c48b0d247094ecd7dacb4070
chrisdorward <chrisdorward@hautla.com>
2020-11-04 03:37:24 +1000

    boilerplate plugin

---
+++ wordpress/wp-content/plugins/jsxform/includes/class-jsxform-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://listingslab.com
+ * @since      1.0.0
+ *
+ * @package    Jsxform
+ * @subpackage Jsxform/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Jsxform
+ * @subpackage Jsxform/includes
+ * @author     listingslab <listingslab@gmail.com>
+ */
+class Jsxform_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ wordpress/wp-content/plugins/jsxform/includes/class-jsxform-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://listingslab.com
+ * @since      1.0.0
+ *
+ * @package    Jsxform
+ * @subpackage Jsxform/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Jsxform
+ * @subpackage Jsxform/includes
+ * @author     listingslab <listingslab@gmail.com>
+ */
+class Jsxform_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
